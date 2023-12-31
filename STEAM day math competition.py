886f8667e11106aa9b601f9e3e84e16adb4d67e9
Feras Sawan <91985496+FerasSawan@users.noreply.github.com>
2023-12-31 18:01:24 -0500

    STEAM day math competition.py

--- STEAM day math competition.py
+++ STEAM day math competition.py
@@ -1,3 +1,7 @@
+/** 
+Used as a quick math competion at our schools anual STEAM day. Prompted user a random multiplication question....
+
+
 import random
 import tkinter as tk
 from tkinter import messagebox
